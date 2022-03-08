ab031b353dd89eab4eca84a7a172e4bcf958d565
Sanket Kumar <sanketcharanpahadi@gmail.com>
2022-03-08 21:28:00 +0530

    added cconfig file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      heading: ['Sen', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
